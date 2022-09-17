f5c9c0bf059d84b11faff078b81b88b4c66c11bc
Yasz Avellia <zakamaragames@gmail.com>
2022-09-17 19:29:49 +0700

    add page

--- layouts/header.php
+++ layouts/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="<?= base_url() ?>/assets/img/logo/redicon.webp">
-    <title>Travelokapala - <?= isset($title) ? $title : 'Pergi Ke Tempat Impian Anda Bersama Kami!' ?></title>
+    <title>RedDoorz - <?= isset($title) ? $title : 'Hotel Murah Mulai Dari 99rb Hanya di RedDoorz!' ?></title>
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="<?= base_url() ?>/assets/vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>/assets/css/style.css">
@@ -19,14 +19,13 @@
     </style>
     <div class=" position-fixed top-0 start-0 end-0 navbar navbar-expand-lg text-bg-light shadow-sm">
         <div class="container ">
-            <a class="navbar-brand" href="<?= base_url()  ?>"><img class="img-fluid" src="assets/img/logo/reddoorz.png" alt=""></a>
+            <a class="navbar-brand" href="<?= base_url()  ?>"><img class="img-fluid" src="assets/img/logo/reddoorz.png" width="150px" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
-                    <a class="nav-link" href="index.php?page=login">Login</a>
-                    <a class="nav-link" href="index.php?page=register">Register</a>
+                    <a class="nav-link" href="index.php?page=register">Gabung | Daftar</a>
                 </div>
             </div>
         </div>
